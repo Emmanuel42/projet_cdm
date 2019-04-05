e29c78e7b794b9707b5764a147e449e7a8fb0d14
Emmanuel <degand.emmanuel@orange.fr>
2019-04-05 09:17:30 +0200

    commit materialize

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!--         <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+        <title>Enigme de la Cour des Matières</title>
+
+<!--         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
+        
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+	</head>
+	
+	<body>
+		
+		<div class="container center-align">
+			
+			<div class="title">
+			
+                <h1>Enigme de la Cour des Matières</h1>
+                    
+            </div>
+            
+            <div>
+            <p>But du jeu : 
+            Vous devez trouver 3 mots-clés qui vous donneront le mot de passe nécessaire pour débloquer une vidéo.
+            Pour trouver les mots, vous devrez résoudre 3 énigmes.
+            Vous n'avez pas accès aux bâtiments. Toutes les énigmes peuvent être résolues dans la Cour des Matières.
+            </p>
+            
+            </div>
+            
+            <div>
+            
+            	<p>Avant de commencer, vous devez choisir la couleur de votre équipe.</p>
+            
+            	<a class="waves-effect waves-light btn-large blue" style="margin-bottom : 20px; width: 300px">Equipe Bleue</a>
+            	</br>
+            	<a class="waves-effect waves-light btn-large red" style="margin-bottom : 20px; width: 300px">Equipe Rouge</a>
+            	</br>
+            	<a class="waves-effect waves-light btn-large green" style="margin-bottom : 20px; width: 300px">Equipe Verte</a>
+            	</br>
+            	<a class="waves-effect waves-light btn-large yellow" style="margin-bottom : 20px; width: 300px">Equipe Jaune</a>
+            	</br>
+            	<a class="waves-effect waves-light btn-large purple" style="margin-bottom : 20px; width: 300px">Equipe Violette</a>
+            </div>
+
+		</div>
+	
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+	
+	</body>
+	
+</html>
